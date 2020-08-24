5d67509d6379d02b7510e02ce06f219141d63d86
jbeltrami <joaopbeltrami@gmail.com>
2020-08-24 19:50:14 -0400

    Create pages mark-up and start styling job page

--- functions.php
+++ functions.php
@@ -84,7 +84,8 @@ class StarterSite extends Timber\Site {
 		$context['foo']   = 'bar';
 		$context['stuff'] = 'I am a value set in your functions.php file';
 		$context['notes'] = 'These values are available everytime you call Timber::context();';
-		$context['menu']  = new Timber\Menu();
+		$context['menu']  = new Timber\Menu(3);
+		$context['menu_social']  = new Timber\Menu(2);
 		$context['site']  = $this;
 		return $context;
 	}
